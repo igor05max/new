7d4f7c2a0cfc70d7da3cef4d55adc1478a9093cb
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-22 14:57:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,113 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Привет, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flask import Flask, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from flask import render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from forms.user import RegisterForm
+from forms.login import LoginForm
+from forms.jobs import NewsForm
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    data = db_sess.query(Jobs).all()
+    return render_template("tt.html", data=data)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+
+        user = User()
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.age = form.age.data
+        user.position = form.position.data
+        user.speciality = form.speciality.data
+        user.address = form.address.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/addjob',  methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = Jobs()
+
+        jobs.title = form.title.data
+
+        jobs.team_leader = int(form.id.data)
+        jobs.job = form.job.data
+        jobs.is_finished = form.is_finished.data
+        jobs.work_size = form.work_size.data
+        jobs.collaborators = form.collaborators.data
+
+        db_sess.add(jobs)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('new_jobs.html', title='Добавление новости',
+                           form=form)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi("12345")
-    print("2000")
-    print("1234")
+    main()
